7031c3f5d512968ab8df09f5cbf7c9fcfd2da093
Eaky0 <atakan.kumas03@gmail.com>
2026-03-22 11:21:42 +0100

    Initialized simulator class

---
+++ pipeline/simulation/ltspice_runner.py
@@ -0,0 +1,15 @@
+from PyLTSpice import LTspice
+
+class TopologySimulator():
+    def __init__(self):
+        pass
+
+    def simulate(self, topID):
+        """Simulates a topology by reading its ID and mapping it onto the actual .net file
+        
+        PARAMS:
+        topID <string> : The ID of the topology 
+        """
+        pass
+
+    
