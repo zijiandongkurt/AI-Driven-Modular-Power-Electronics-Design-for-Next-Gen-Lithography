a786805ac82da9fac54d91d7e38e78cc4afa9742
Eaky0 <atakan.kumas03@gmail.com>
2026-05-05 15:59:43 +0200

    Normalized reward function tested -> works!

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from pipeline.llm_topology_generation.llm_api import TopologyLLM
 from pipeline.netlist_validation.validator import validator
 from pipeline.simulation.ltspice_runner import TopologySimulator
 from pipeline.reward_evaluation.reward_function import RewardFunction
+from pipeline.reward_evaluation.reward_function_norm import RewardFunctionNorm
 from pipeline.llm_topology_generation.prompt_input import load_constraint
-
+from pipeline.reinforcement_algorithm.grpo_trainer import GRPOTrainer
+from pipeline.llm_topology_generation.rl_updater import RLUpdater, RLConfig
 
 def main():
     llm        = TopologyLLM(model_id="Qwen/Qwen3-14B")
     val        = validator()
     simulator  = TopologySimulator()
-    reward_fn  = RewardFunction()
+    reward_fn  = RewardFunctionNorm()
+    grpo = GRPOTrainer()
+    rl_updater = RLUpdater(config=RLConfig())
     constraint = load_constraint("pipeline/data/datasets/constraints.json", idx=0)
-    batch_id   = "batch_8"
+    batch_id   = "batch_1"
 
     # 1. Generate — writes .net files to data/batch_7/llm_output/
-    #written = llm.generate_for_batch(constraint, batchID=batch_id, n=4)
-    #print(f"Generated {len(written)} netlists")
+    written = llm.generate_for_batch(constraint, batchID=batch_id, n=4)
+    print(f"Generated {len(written)} netlists")
 
     # 2. Validate — reads llm_output/, writes validation_results.json
     val.validate(batch_id)
@@ -31,6 +35,8 @@ def main():
         "components": {"mosfet": 1.0, "diode": 1.0,
                     "inductor": 1.0, "capacitor": 1.0}
     })
+
+    
 
 
 if __name__ == "__main__":
